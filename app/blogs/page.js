af50b924df3e752d7831c0cda9506a227195f675
Myo-Htet-San-Brainy <myohtetsandrinksgit@gmail.com>
2024-10-03 21:29:09 +0700

    blogs section/page almost done, waiting for some sample blog data

--- app/blogs/page.js
+++ app/blogs/page.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { getBlogs } from "../lib/dataFetches";
 import SectionTitle from "../ui/SectionTitle";
+import BlogCard from "../ui/BlogCard";
 
 const page = async () => {
   const blogs = await getBlogs();
@@ -10,6 +11,11 @@ const page = async () => {
   return (
     <main>
       <SectionTitle text={"Blogs"} color={"text-orange-400"} />
+      <div className="w-[80%] max-w-screen-xl mx-auto pt-12 grid gap-4 md:grid-cols-2 lg:grid-cols-3 ">
+        {blogs.map((blog) => {
+          return <BlogCard key={blog._id} {...blog} />;
+        })}
+      </div>
     </main>
   );
 };
---
+++ app/ui/BlogCard.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { getMonthDiff } from "../lib/utils/monthDiff";
+import { trimText } from "../lib/utils/trimText";
+
+const BlogCard = ({ name, text, img, readTime, createdAt }) => {
+  const monthsAgo = getMonthDiff(createdAt);
+  return (
+    <div className="card w-full bg-base-100 shadow-xl hover:shadow-2xl transition-all duration-300 bg-orange-400">
+      <figure>
+        <img src={img} alt={name} />
+      </figure>
+      <div className="card-body text-white">
+        <div className=" flex justify-between text-sm">
+          <h1>{readTime}</h1>
+          <h1>
+            {monthsAgo === 0 ? "Just moment ago" : `${monthsAgo} months ago`}
+          </h1>
+        </div>
+        <h2 className="mt-2 h-16 card-title font-bold">
+          {trimText(name) ? trimText(name) + " . . ." : name}
+        </h2>
+        <p className="font-light text-lg tracking-wide">
+          {trimText(text) ? trimText(text, 200) + " . . ." : text}
+        </p>
+        <button className="btn mt-2 text-orange-500 bg-white text-base hover:text-white hover:bg-orange-300 transition-all duration-300 border-none">
+          Read More
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default BlogCard;
